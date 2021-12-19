c5de4605800dda837dd0f099f0fedac7a959c3b5
opyton <36433925+opyton@users.noreply.github.com>
2021-12-19 17:13:35 -0500

    Add files via upload

---
+++ Gymnstocks/src/main/java/com/Gymnstocks/GymnstocksApplication.java
@@ -0,0 +1,13 @@
+package com.Gymnstocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnstocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymnstocksApplication.class, args);
+	}
+
+}
---
+++ Gymnstocks/src/test/java/com/Gymnstocks/GymnstocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gymnstocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnstocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
